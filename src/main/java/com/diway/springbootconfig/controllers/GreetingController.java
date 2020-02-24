791193899e5f60017e9b2f0885d274d9a10d8584
Diway Sanu <diway.sanu@team.telstra.com>
2020-02-25 08:20:56 +1100

    First commit

---
+++ src/main/java/com/diway/springbootconfig/controllers/GreetingController.java
@@ -0,0 +1,46 @@
+package com.diway.springbootconfig.controllers;
+
+import com.diway.springbootconfig.config.DbSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GreetingController {
+
+    // Providing default value
+    @Value("${my.greeting: default value}")
+    private String greetingMessage;
+
+    @Value("some static message")
+    private String staticMessage;
+
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+    // # tells to evaluate using SPEL (Spring Expression Language)
+    @Value("#{${dbValues}}")
+    private Map<String, String> dbValues;
+
+    @Autowired
+    private DbSettings dbSettings;
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        //return greetingMessage + staticMessage + listValues + dbValues;
+        return dbSettings.getConnection() + dbSettings.getHost();
+    }
+
+    @GetMapping("/envdetails")
+    public String getEnvDetails() {
+        return env.toString();
+    }
+}
